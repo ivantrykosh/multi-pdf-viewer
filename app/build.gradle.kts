0e96ffdeba873f9db09aea5593f5df7334053f1c
Ivan Trykosh <trykoshivan4@gmail.com>
2026-07-18 15:49:18 +0300

    v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ivantrykosh.app.multipdfviewer"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
